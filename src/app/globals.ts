1bdf21386d18f36eceffdf5e84b4f568f2dbbd8f
JavieRodriguez <javier890302@gmail.com>
2018-10-25 22:28:15 -0500

    Login
    Se ajusta el login y el inicio.

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,7 +5,7 @@ export class Globals {
   //url:string = 'https://contratacion.herokuapp.com/';
   url: string = 'http://localhost:8080';
   ADMINISTRADOR:string = '0';
-  VECINO:string = '1';
+  USUARIO:string = '1';
   ALMACEN:string = '2';
   RECOLECTOR:string = '3';
   imgdefault:string = 'assets/img/default/44_supermarket_cart_shopping_item_add_product-512.png';
