c3be7f3a23b19b90bc45d4f8b155c7c918960944
JavieRodriguez <javier890302@gmail.com>
2018-10-27 14:52:07 -0500

    20181027
    Se crea compania service y vistas de crear varios CRUD

--- src/app/services/companias.service.ts
+++ src/app/services/companias.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Globals } from '../globals';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniasService {
 
-  constructor() { }
+  url:string;
+  encabezados:any;
+  Companias:Array<any>;
+
+  constructor(private http:HttpClient,
+    private _globals:Globals) {
+    this.url = _globals.url + '/compania';
+  	this.encabezados = {
+  		headers: new HttpHeaders(
+		  	{
+		  		"Content-Type": "application/json"//,
+		  		//"Authorization": "Bearer " + localStorage.getItem("SessionToken")
+		  	}
+	  	)
+  	};
+  }
+
+  /* GET index */
+  traerCompanias():Observable<any>{
+  	return this.http.get<any>(
+  			this.url,
+  			this.encabezados
+  	);
+  }
+
+  /* GET show */
+  mostrarCompania(id):Observable<any>{
+  	let urlcompania = this.url + "/" + id;
+  	return this.http.get<any>(
+  		urlcompania,
+  		this.encabezados
+  	);
+  }
+
+  /* POST create */
+  crearcompania(compania){
+  	return this.http.post<any>(
+  		this.url,
+  		compania,
+  		this.encabezados
+  	);
+  }
+
+  /* PUT update */
+  modificarcompania(compania){
+	let urlcompania = this.url;
+  	return this.http.put<any>(
+  		urlcompania,
+  		compania,
+  		this.encabezados
+  	);
+  }
+
+  /* DELETE destroy */
+  eliminarcompania(id){
+  	let urlcompania = this.url + "/" + id;
+  	return this.http.delete<any>(
+  		urlcompania,
+  		this.encabezados
+  	);
+  }
 }
