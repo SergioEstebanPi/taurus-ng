8c2fae797c43a64a6e83e71ad5c1031c2f4ac401
SergioEstebanPi <sepi_147@hotmail.com>
2018-10-27 15:43:59 -0500

    Corrijo rutas de cruds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,47 +2,54 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
 import { Globals } from './globals';
+import { RouterModule, Routes} from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { PiedepaginaComponent } from './piedepagina/piedepagina.component';
 import { AcercadeComponent } from './acercade/acercade.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
-import { ModalidadesComponent } from './modalidades/modalidades.component';
-import { CrearModalidadComponent } from './crear-modalidad/crear-modalidad.component';
-import { EditarModalidadComponent } from './editar-modalidad/editar-modalidad.component';
 
-import { RouterModule, Routes} from '@angular/router';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { ProcesosComponent } from './procesos/procesos.component';
 import { CrearProcesoComponent } from './crear-proceso/crear-proceso.component';
 import { EditarProcesoComponent } from './editar-proceso/editar-proceso.component';
-import { VerModalidadComponent } from './ver-modalidad/ver-modalidad.component';
 import { VerProcesoComponent } from './ver-proceso/ver-proceso.component';
+
+import { ModalidadesComponent } from './modalidades/modalidades.component';
+import { CrearModalidadComponent } from './crear-modalidad/crear-modalidad.component';
+import { EditarModalidadComponent } from './editar-modalidad/editar-modalidad.component';
+import { VerModalidadComponent } from './ver-modalidad/ver-modalidad.component';
+
 import { CompaniasComponent } from './companias/companias.component';
 import { CrearCompaniaComponent } from './crear-compania/crear-compania.component';
 import { EditarCompaniaComponent } from './editar-compania/editar-compania.component';
 import { VerCompaniaComponent } from './ver-compania/ver-compania.component';
+
 import { FormadepagosComponent } from './formadepagos/formadepagos.component';
 import { CrearFormadepagoComponent } from './crear-formadepago/crear-formadepago.component';
 import { EditarFormadepagoComponent } from './editar-formadepago/editar-formadepago.component';
 import { VerFormadepagoComponent } from './ver-formadepago/ver-formadepago.component';
+
 import { ParametrosComponent } from './parametros/parametros.component';
 import { CrearParametroComponent } from './crear-parametro/crear-parametro.component';
 import { EditarParametroComponent } from './editar-parametro/editar-parametro.component';
 import { VerParametroComponent } from './ver-parametro/ver-parametro.component';
+
 import { TipocontratosComponent } from './tipocontratos/tipocontratos.component';
 import { CrearTipocontratoComponent } from './crear-tipocontrato/crear-tipocontrato.component';
 import { EditarTipocontratoComponent } from './editar-tipocontrato/editar-tipocontrato.component';
 import { VerTipocontratoComponent } from './ver-tipocontrato/ver-tipocontrato.component';
+
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
 import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
 import { VerUsuarioComponent } from './ver-usuario/ver-usuario.component';
+
 import { GarantiasComponent } from './garantias/garantias.component';
 import { CrearGarantiaComponent } from './crear-garantia/crear-garantia.component';
 import { EditarGarantiaComponent } from './editar-garantia/editar-garantia.component';
@@ -66,32 +73,32 @@ const rutas:Routes=[
   {path:"editar-proceso/:id", component:EditarProcesoComponent},  
   {path:"ver-proceso/:id", component:VerProcesoComponent},
   
-  {path:"companias", component:CrearCompaniaComponent},
+  {path:"companias", component:CompaniasComponent},
   {path:"crear-compania", component:CrearCompaniaComponent},
   {path:"editar-compania/:id", component:EditarCompaniaComponent},
   {path:"ver-compania/:id", component:VerCompaniaComponent},
 
-  {path:"formadepagos", component:CrearFormadepagoComponent},
+  {path:"formadepagos", component:FormadepagosComponent},
   {path:"crear-formadepago", component:CrearFormadepagoComponent},
   {path:"editar-formadepago/:id", component:EditarFormadepagoComponent},
   {path:"ver-formadepago/:id", component:VerFormadepagoComponent},
 
-  {path:"parametros", component:CrearParametroComponent},
+  {path:"parametros", component:ParametrosComponent},
   {path:"crear-parametro", component:CrearParametroComponent},
   {path:"editar-parametro/:id", component:EditarParametroComponent},
   {path:"ver-parametro/:id", component:VerParametroComponent},
 
-  {path:"tipocontratos", component:CrearTipocontratoComponent},
+  {path:"tipocontratos", component:TipocontratosComponent},
   {path:"crear-tipocontrato", component:CrearTipocontratoComponent},
   {path:"editar-tipocontrato/:id", component:EditarTipocontratoComponent},
   {path:"ver-tipocontrato/:id", component:VerTipocontratoComponent},
 
-  {path:"usuarios", component:CrearUsuarioComponent},
+  {path:"usuarios", component:UsuariosComponent},
   {path:"crear-usuario", component:CrearUsuarioComponent},
   {path:"editar-usuario/:id", component:EditarUsuarioComponent},
   {path:"ver-usuario/:id", component:VerUsuarioComponent},
 
-  {path:"garantias", component:CrearGarantiaComponent},
+  {path:"garantias", component:GarantiasComponent},
   {path:"crear-garantia", component:CrearGarantiaComponent},
   {path:"editar-garantia/:id", component:EditarGarantiaComponent},
   {path:"ver-garantia/:id", component:VerGarantiaComponent},  
--- src/app/crear-proceso/crear-proceso.component.ts
+++ src/app/crear-proceso/crear-proceso.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {  ProcesosService } from '../services/procesos.service';
+import {  CompaniasService } from '../services/companias.service';
 import { Router } from '@angular/router';
 import { Globals } from '../globals';
 
@@ -12,9 +13,11 @@ import { Globals } from '../globals';
 export class CrearProcesoComponent implements OnInit {
 
 	proceso:any;
+  compania:any;
   error:boolean;
 
-  constructor(private _procesoes:ProcesosService,
+  constructor(private _procesos:ProcesosService,
+    private _companias:CompaniasService,
   			private _router:Router,
             private _globals:Globals) {
   	this.proceso =     {
@@ -31,10 +34,33 @@ export class CrearProcesoComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.traerCompanias();
+  }
+
+  traerCompanias(){
+    this._companias.traercompanias()
+      .subscribe(
+        respuesta => {
+          if (this.frase && this.frase.trim() != '') {
+            this.companias = this.companias.filter(
+              (item) => {
+                return (item.nombre.toLowerCase().indexOf(this.frase.toLowerCase()) > -1);
+              }
+            );
+          } else {
+            this.companias = respuesta;
+          }
+          this._router.navigate(['/modalidades']);
+          //this.modalidades = respuesta;
+        },
+        error => {
+          console.log(error);
+        }
+      );    
   }
 
   crearproceso(){
-		this._procesoes.crearproceso(this.proceso)
+		this._procesos.crearproceso(this.proceso)
 			.subscribe(
 				respuesta => {
           this.error = false;
