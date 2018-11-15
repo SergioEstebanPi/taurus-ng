5d5f69c2d41cc5e6b4a194100023dcf439672c9f
SergioEstebanPi <sepi_147@hotmail.com>
2018-11-15 06:52:24 -0500

    Agrego validación de roles de usuario y agrego reporte segun proceso

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  url:string = 'https://contratacion.herokuapp.com/';
+  url:string = 'https://contratacion.herokuapp.com';
   //url: string = 'http://localhost:8080';
   ADMINISTRADOR:string = '0';
   USUARIO:string = '1';
